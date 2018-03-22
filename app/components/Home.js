8c2106ada899deefa6c7f71958aed73ca331ebdd
James Proctor <jamesproctor_Pitch@Jamess-MacBook-Pro.local>
2018-03-22 15:19:15 -0700

    newproject page approx up to date with wireframes

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,14 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-
-const path = require('path');
-const { remote, clipboard } = require('electron');
-const { dialog } = remote;
-const { spawn, execFile } = require('child_process');
-
-const isDev = () => process.env.NODE_ENV === 'development';
-const appPath = isDev() ? __dirname : remote.app.getAppPath();
 
 import { getProjects, getSamples } from '../projects.js';
 import ProjectList from './ProjectList.js';
@@ -21,73 +12,12 @@ export default class Home extends Component {
     super(props);
 
     this.state = {
-      filename: '',
-      datainfo: '',
       projects: getProjects(),
       samples: getSamples(),
     };
-
-    this.showFilename = this.showFilename.bind(this);
-    this.showFileInfo = this.showFileInfo.bind(this);
-    this.onChosenFileToOpen = this.onChosenFileToOpen.bind(this);
-    this.handleOpenButton = this.handleOpenButton.bind(this);
-  }
-
-  showFilename(filepath) {
-    const filename = filepath.toString().split('/');
-    this.setState({filename:filename[filename.length-1]});
-  }
-
-  uint8arrayToString(data){
-    return String.fromCharCode.apply(null, data);
-  };
-
-  displayInfo(data) {
-    const datainfo = JSON.stringify(data);
-    this.setState({datainfo:datainfo});
-  }
-
-  showFileInfo(filepath) {
-    const python = spawn(path.join(`${appPath}`,`/../biomhandler/dist/biomhandler`), [filepath]);
-    let json = '';
-    python.stdout.on('data', (data) => {
-      json += this.uint8arrayToString(data);
-    });
-    python.stdout.on('end', () => {
-      const data = JSON.parse(json);
-      const shallowData = {};
-      Object.keys(data).forEach((k) => {
-        if ((typeof data[k] === 'number') || (typeof data[k] === 'string')) {
-          shallowData[k] = data[k];
-        }
-      });
-      this.displayInfo(shallowData);
-    });
-    python.stderr.on('data', (data) => {
-      console.warn(this.uint8arrayToString(data));
-    });
-  }
-
-  onChosenFileToOpen(filepath) {
-    this.showFilename(filepath);
-    this.showFileInfo(filepath);
-  }
-
-  handleOpenButton() {
-    dialog.showOpenDialog({properties: ['openFile']}, (filepath) => {
-      if (filepath) {
-        this.onChosenFileToOpen(filepath.toString());
-      }
-    });
   }
 
   render() {
-    // TODO: break down into componenets
-    /*
-            <h3>{this.state.filename}</h3>
-            <button id='open' onClick={this.handleOpenButton}>Choose a file</button>
-            <div>{this.state.datainfo}</div>
-    */
     const links = LinkList();
     const projects = ProjectList(this.state.projects);
     const samples = ProjectList(this.state.samples);
