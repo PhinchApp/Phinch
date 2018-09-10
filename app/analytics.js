a798a0d27325b74b20e4a18a58680239b1d98e25
James Proctor <james@pitchinteractive.com>
2018-09-10 14:35:20 -0400

    fixed analytics local storage issue

--- app/analytics.js
+++ app/analytics.js
@@ -1,15 +1,15 @@
 import ua from 'universal-analytics';
 import uuid from 'uuid';
+import { remote } from 'electron';
 import { JSONStorage } from 'node-localstorage';
 
 // ref: https://kilianvalkhof.com/2018/apps/using-google-analytics-to-gather-usage-statistics-in-electron/
 
-// const nodeStorage = new JSONStorage('./phinch');
-// const visitorId = nodeStorage.getItem('visitorid') || uuid();
-// nodeStorage.setItem('visitorid', visitorId);
+const nodeStorage = new JSONStorage(remote.app.getPath('userData'));
+const visitorId = nodeStorage.getItem('visitorid') || uuid();
+nodeStorage.setItem('visitorid', visitorId);
 
-// const visitor = ua('UA-50346302-2', visitorId);
-const visitor = ua('UA-50346302-2');
+const visitor = ua('UA-50346302-2', visitorId);
 
 export function trackEvent(category, action, label, value) {
   visitor.event({
