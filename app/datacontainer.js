bd952aa6d94f282864d0ca4a02dd006757f1e9ac
James Proctor <james@pitchinteractive.com>
2019-02-12 17:11:50 -0800

    moved workers outside app to separate stub `module` to enable use in packaged app

--- app/datacontainer.js
+++ app/datacontainer.js
@@ -4,8 +4,9 @@ import { join, resolve } from 'path';
 
 const isDev = () => process.env.NODE_ENV === 'development';
 const appPath = isDev() ? __dirname : remote.app.getAppPath();
-const biomhandlerPath = join(appPath, '/..', '/biomhandler', '/dist', '/biomhandler');
-const worker = new Worker(resolve(appPath, 'workers', 'loadAndFormatData.js'));
+const biomhandlerPath = resolve(appPath, '..', 'biomhandler', 'dist', 'biomhandler');
+const workerPath = resolve(appPath, '..', 'workers', 'loadAndFormatData.js');
+const worker = new Worker(workerPath);
 
 // Maybe externalize this to utils or something
 function formatFileSize(bytes) {
@@ -63,7 +64,7 @@ class DataContainer {
 
   loadAndFormatData(filepath, success, failure) {
     // const worker = new Worker(resolve(appPath, 'workers', 'loadAndFormatData.js'));
-    // 
+    //
     worker.postMessage({ biomhandlerPath, filepath });
     worker.onmessage = e => {
       if (e.data.status === 'success') {
